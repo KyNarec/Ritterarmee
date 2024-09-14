244076209a9a41657af3787a5acf61702b58f86b
Simon-Sonnleitner <jim48@gmx.de>
2024-09-14 11:28:30 +0200

    just continued

--- 0_Ritterarmee_Vorlage/AUFMARSCH.java
+++ 0_Ritterarmee_Vorlage/AUFMARSCH.java
@@ -1,9 +1,11 @@
+import java.util.Random;
 public class AUFMARSCH extends SPIEL
 {
     int zaehler = 0;
     RITTER[] armee;
 
     public AUFMARSCH(){
+        super(960,540);
         zaehler++;
         armee = new RITTER[8];
         super.starteTickerNeu(1);
@@ -18,14 +20,41 @@ public class AUFMARSCH extends SPIEL
     }
 
     public void einordnen(RITTER ritter){
-
+        int pos = freiePositionFinden();
+        //TODO: hier weiter machen bei S.6 Arbeitsauftrag 2 b
+        ritter.marschieren(pos);
     }
-    public int freiePositioinFinden(){
 
-        return 1;
+    public int freiePositionFinden(){
+        for (int i = 0; i < armee.length; i++) {
+            if (armee[i] != null) {
+                return i-1;
+            }
+        }
+        return 0;
     }
 
     @Override
-    public void tick() {
+    public void tick(){
+        zaehler++;
+        String nBild = "";
+        Random rand = new Random();
+        int zufallszahl = rand.nextInt(4);
+        switch (zufallszahl){
+            case 0:
+                nBild = "ritter_1.png";
+                break;
+            case 1:
+                nBild = "ritter_2.png";
+                break;
+            case 2:
+                nBild = "ritter_3.png";
+                break;
+            case 3:
+                nBild = "ritter_4.png";
+                break;
+        }
+        String nName = "Olaf"+zaehler;
+        RITTER ritter_neu = new RITTER(-3,-4,nBild,nName);
     }
 }
--- 0_Ritterarmee_Vorlage/main.java
+++ 0_Ritterarmee_Vorlage/main.java
@@ -1,7 +1,5 @@
 public class main {
     public static void main(String[] args) {
-        RITTER ritter = new RITTER(5, 5, "ritter_1.png", "Bob");
         AUFMARSCH aufmarsch = new AUFMARSCH();
-
     }
 }
